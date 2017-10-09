6bef02f2cd554f7bdba41486d49ae93731e86a67
emadelmogy <emadelmogy619@gmail.com>
2017-10-09 12:29:17 +0200

    add readme file

--- ytc/youtubeCrawler/views.py
+++ ytc/youtubeCrawler/views.py
@@ -12,6 +12,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    yt = YouTube("https://www.youtube.com/watch?v=qE0JZUKRz1A")
+    yt = YouTube("https://www.youtube.com/user/AsapSCIENCE/videos")
     videos = yt.get_videos()
     return render(request, 'home.html', {'videos': videos})
